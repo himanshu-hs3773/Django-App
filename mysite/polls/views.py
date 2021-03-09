cc452e00981491c27553bf102a54d6c336ba0e82
himanshu-hs3773 <hs3773@nyu.edu>
2021-03-09 14:51:50 -0800

    Added filter for limiting access to future posts & there test cases

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,14 +12,19 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        """Return the last five published questions till date."""
         # return Question.objects.order_by('-pub_date')[:5]
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]      #__lte less than or equal to
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -41,50 +46,3 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]			#-pub_date gives descending order
-#     #template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     #return HttpResponse(template.render(context, request))
-#     return render(request, 'polls/index.html', context)							# django shortcut to loader/httpresponse.loader
-
-# def detail(request, question_id):
-#     # try:
-#     #    question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #    raise Http404("Question does not exist")
-
-#     question = get_object_or_404(Question, pk=question_id)						# djando shortcut for try/catch / django.http Http404 module
-
-#     # The get_list_or_404() function uses filter() instead of get() and raises Http404 if the list is empty
-    
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     #response = "You're looking at the results of question %s."
-#     question = get_object_or_404(Question, pk=question_id)
-#     #return HttpResponse(response % question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-# #     return HttpResponse("You're voting on question %s." % question_id)
-
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])    # request.POST values are string
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
